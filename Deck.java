890d9c400032e8fa60f8cac8e26df8ee4dec57d1
Hong Tai Wei <weihongtai@hotmail.com>
2016-01-16 16:44:24 -0500

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,45 @@
+package game;
+
+import java.util.Stack;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deck {
+	
+	private int nbr;
+	private Stack<Integer> cards;
+	
+	public Deck (int nbr) {
+		this.nbr = nbr;
+		cards = new Stack<Integer>();
+		
+		for (int i = 0; i < nbr/4; i++) {
+			cards.push(2);
+			cards.push(3);
+			cards.push(4);
+			cards.push(5);
+		}
+		
+	}
+	
+	public int getNbr () {
+		return nbr;
+	}
+	
+	public void shuffle () {
+		Collections.shuffle(cards);
+	}
+	
+	public int topDeck () {
+		return cards.pop();
+	}
+	
+	public void putBack (int c) {
+		cards.push(c);
+	}
+	
+	public String deckList () {
+		return Arrays.toString(cards.toArray());
+	}
+
+}
